dc00a1b431ef74ef407466826d8595740639e031
Monty Anderson <monty.linux@gmail.com>
2016-09-20 11:16:43 +0100

    browserify

--- Expression.js
+++ Expression.js
@@ -10,6 +10,38 @@ function count(arr, item) {
 	return c;
 }
 
+function compare(arr0, arr1) {
+	if(arr0.length !== arr1.length) return false;
+
+	arr0 = arr0.slice().sort();
+	arr1 = arr1.slice().sort();
+
+	for(let i = 0; i < arr0.length; i++) {
+		if(arr0[i] !== arr1[i]) return false;
+	}
+
+	return true;
+}
+
+function add(arr) {
+	let n = 0;
+
+	arr.forEach(a => n += a);
+
+	return n;
+}
+
+function times(arr) {
+	let n = 1;
+
+	arr.forEach(a => n *= a);
+
+	return n;
+}
+
+const isNumeric = (a) => typeof a === "number";
+const isSymbolic = (a) => typeof a === "string";
+
 module.exports = class Expression {
 	constructor(str) {
 		this.terms = str.replace(new RegExp(" ", "g"), "").split("+").map((str) => {
@@ -35,17 +67,33 @@ module.exports = class Expression {
 		this.terms.forEach(term => {
 			let n = 1;
 
-			term.filter(a => typeof a == "number").forEach((a, i) => {
+			term.filter(isNumeric).forEach((a, i) => {
 				n *= a;
 				term.splice(i, 1);
 			});
 
 			term.unshift(n);
 		});
+
+		this.terms.forEach(a => {
+			this.terms.forEach((b, i) => {
+				if(a === b) return;
+
+				if(compare(a.filter(isSymbolic), b.filter(isSymbolic))) {
+					const n = b.filter(isNumeric)[0];
+
+					console.log(n);
+
+					a[a.indexOf(a.filter(isNumeric)[0])] += n;
+
+					this.terms.splice(i, 1);
+				}
+			});
+		});
 	}
 
 	multiply(n) {
-		this.terms.forEach((term) => {
+		this.terms.forEach(term => {
 			term.push(n);
 		});
 
